6166871967a07697a22d20d462137cd9b02ece03
Yasin-Yilmazz <yasinnyilmazz26@gmail.com>
2022-06-07 13:46:24 +0300

    guess number end

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const between = document.getElementById("between");
 const guessInput = document.getElementById("guess-input");
 const submitBtn = document.getElementById("submitBtn");
-const guesses = document.querySelector(".guesses");
+const oldGuesses = document.querySelector(".guesses");
 const remainingGuess = document.querySelector(".remainingGuess");
+const startOver = document.querySelector(".resultParas");
 const lowOrHi = document.querySelector(".lowOrHi");
 let randInt = Math.floor(Math.random() * 100) + 1;
 const form = document.querySelector("form");
-// let previousGuesses = [];
-let numGuess = 4;
+const restart = document.createElement("p");
+
+let numGuess = 5;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -23,10 +25,10 @@ const validateGuess = (guess) => {
   } else if (guess > 100) {
     alert("Please enter a number less than 100!");
   } else {
-    // previousGuesses.push(guess);
-
-    if (numGuess < 1) {
+    if (numGuess === 1) {
+      displayGuesses(guess);
       lowOrHi.innerHTML = `Game over! Number was ${randInt}`;
+      endGame();
     } else {
       displayGuesses(guess);
       checkGuess(guess);
@@ -37,6 +39,7 @@ const validateGuess = (guess) => {
 const checkGuess = (g) => {
   if (g === randInt) {
     lowOrHi.innerHTML = `Congrats You Win!!`;
+    endGame();
   } else if (g < randInt) {
     lowOrHi.innerHTML = `Go higher!`;
   } else if (g > randInt) {
@@ -46,14 +49,29 @@ const checkGuess = (g) => {
 
 const displayGuesses = (gues) => {
   guessInput.value = "";
-  guesses.innerHTML += `${gues} `;
-  remainingGuess.innerHTML = `${numGuess}`;
+  oldGuesses.innerHTML += `${gues} `;
   numGuess--;
+  remainingGuess.innerHTML = `${numGuess}`;
 };
 
 const endGame = () => {
+  guessInput.value = "";
   guessInput.setAttribute("disabled", "");
-  const p = document.createElement("p");
-  p.setAttribute("class", "button");
-  p.innerHTML = ``;
+
+  restart.innerHTML = `<button type="submit" class="btn btn-outline-success mt-3 mb-2" id = "newGame" >Start New Game</button>`;
+  startOver.appendChild(restart);
+  newGame();
+};
+
+const newGame = () => {
+  const newGameButton = document.querySelector("#newGame");
+  newGameButton.addEventListener("click", function () {
+    randInt = parseInt(Math.random() * 100 + 1);
+    numGuess = 5;
+    remainingGuess.innerHTML = numGuess;
+    oldGuesses.innerHTML = "";
+    lowOrHi.innerHTML = "";
+    guessInput.removeAttribute("disabled");
+    startOver.removeChild(restart);
+  });
 };
